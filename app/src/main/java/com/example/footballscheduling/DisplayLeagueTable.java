7102d1bb507029fb02c5ab0c9afbd4557cfb9f93
LEONE <leonwahungu@gmail.com>
2021-09-21 11:43:35 +0300

    -git commit 'commit'

--- app/src/main/java/com/example/footballscheduling/DisplayLeagueTable.java
+++ app/src/main/java/com/example/footballscheduling/DisplayLeagueTable.java
@@ -1,11 +1,16 @@
 package com.example.footballscheduling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayLeagueTable extends AppCompatActivity {
     Toolbar toolbar;
@@ -29,5 +34,24 @@ public class DisplayLeagueTable extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sort_by_points, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sortByPoints:
+                Toast.makeText(this, "Sort by points selected", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
