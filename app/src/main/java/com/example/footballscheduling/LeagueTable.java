4381b5fff7c96fa9309aea23fae3d20e877a2383
LEONE <leonwahungu@gmail.com>
2021-09-27 09:56:40 +0300

    -git commit 'commit'

--- app/src/main/java/com/example/footballscheduling/LeagueTable.java
+++ app/src/main/java/com/example/footballscheduling/LeagueTable.java
@@ -1,24 +1,11 @@
 package com.example.footballscheduling;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import org.jetbrains.annotations.NotNull;
-import java.util.ArrayList;
 
 public class LeagueTable extends AppCompatActivity {
     Toolbar toolbar;
-    RecyclerView recyclerView;
-    private AdapterTeam adapterTeam;
-    private ArrayList<Teams> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +13,7 @@ public class LeagueTable extends AppCompatActivity {
         setContentView(R.layout.activity_league_table);
 
         toolbar = findViewById(R.id.toolbar);
-        recyclerView = findViewById(R.id.recyclerview);
 
         setSupportActionBar(toolbar);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        recyclerView.setHasFixedSize(true);
-
-        list = new ArrayList<>();
-
-        Query query = FirebaseDatabase.getInstance().getReference().child("Teams");
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
-                    Teams model = dataSnapshot.getValue(Teams.class);
-                    list.add(model);
-                }
-                adapterTeam = new AdapterTeam(LeagueTable.this, list);
-                recyclerView.setAdapter(adapterTeam);
-                adapterTeam.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
     }
 }
