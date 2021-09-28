3e7fbf19b7092b337ac671c2def116ce310b0a7c
LEONE <leonwahungu@gmail.com>
2021-09-28 15:47:27 +0300

    -git commit 'commit'

---
+++ app/src/main/java/com/example/footballscheduling/AdapterResults.java
@@ -0,0 +1,67 @@
+package com.example.footballscheduling;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AdapterResults extends RecyclerView.Adapter<AdapterResults.MyViewHolder> {
+    ArrayList<ArrayList<Map>> mList;
+    Context context;
+
+    public AdapterResults(Context context, ArrayList<ArrayList<Map>> mList){
+        this.mList = mList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public AdapterResults.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.results_view, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        ArrayList<Map> round = mList.get(position);
+        Map<String, Object> match1 = round.get(0);
+        Map<String, Object> match2 = round.get(1);
+        Map<String, Object> match3 = round.get(2);
+
+        holder.round.setText("Round " + (position + 1));
+
+        holder.team1.setText(match1.get("home").toString());
+        holder.team2.setText(match1.get("away").toString());
+        holder.team3.setText(match2.get("home").toString());
+        holder.team4.setText(match2.get("away").toString());
+        holder.team5.setText(match3.get("home").toString());
+        holder.team6.setText(match3.get("away").toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView team1, team2, team3, team4, team5, team6, round;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            team1 = itemView.findViewById(R.id.team1);
+            team2 = itemView.findViewById(R.id.team2);
+            team3 = itemView.findViewById(R.id.team3);
+            team4 = itemView.findViewById(R.id.team4);
+            team5 = itemView.findViewById(R.id.team5);
+            team6 = itemView.findViewById(R.id.team6);
+            round = itemView.findViewById(R.id.round);
+        }
+    }
+}
--- app/src/main/java/com/example/footballscheduling/ManageLeagueTable.java
+++ app/src/main/java/com/example/footballscheduling/ManageLeagueTable.java
@@ -1,14 +1,72 @@
 package com.example.footballscheduling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
 import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ManageLeagueTable extends AppCompatActivity {
+    private AdapterResults adapterResults;
+    private RecyclerView recyclerView;
+    private ArrayList<ArrayList<Map>>  list;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_league_table);
+
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.recyclerview);
+
+        setSupportActionBar(toolbar);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("Fixtures");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                    ArrayList<Map> matches = new ArrayList<>();
+                    for (DataSnapshot match : snapshot1.getChildren()){
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("home", match.child("home").getValue());
+                        map.put("away", match.child("away").getValue());
+                        matches.add(map);
+                    }
+                    list.add(matches);
+                }
+                adapterResults = new AdapterResults(ManageLeagueTable.this, list);
+                recyclerView.setAdapter(adapterResults);
+                adapterResults.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(ManageLeagueTable.this, Home.class);
+        startActivity(intent);
+        finish();
     }
 }
